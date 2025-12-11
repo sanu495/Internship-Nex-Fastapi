ac4a30c224f7d4d75007918a089a0b77ed2d6e16
Sànu 🦋 <sanoops658@gmail.com>
2025-12-11 16:12:39 +0530

    Done

--- JinjaController/StudentJinja.py
+++ JinjaController/StudentJinja.py
@@ -25,7 +25,7 @@ def home(request:Request):
 def get_student_home(request:Request):
     return templates.TemplateResponse("studenthome.html",{"request":request})
 
-# STUDENT HOME PAGE (REQUEST)
+# STUDENT HOME PAGE (RESPONSE)
 
 @apirouter.get("/Studentform", name="student_form", response_class=HTMLResponse)
 def get_student_form(request:Request):
@@ -36,14 +36,28 @@ def get_student_update(request:Request):
     return templates.TemplateResponse("Studentupdate.html",{"request":request})
 
 @apirouter.get("/Studentclass", name="student_class", response_class=HTMLResponse)
-def get_student_update(request:Request):
+def get_student_class(request:Request):
     return templates.TemplateResponse("StudentClass.html",{"request":request})
 
 @apirouter.get("/Studentcontact", name="student_contact", response_class=HTMLResponse)
-def get_student_update(request:Request):
+def get_student_contact(request:Request):
     return templates.TemplateResponse("StudentContact.html",{"request":request})
 
-# STUDENT PAGE (RESPONSE)
+@apirouter.get("/Studentsearch", name="student_search", response_class=HTMLResponse)
+def get_student_id(request:Request):
+    return templates.TemplateResponse("search.html",{"request":request})
+
+@apirouter.get("/error", name="student_error", response_class=HTMLResponse)
+def error_message(request:Request):
+    return templates.TemplateResponse("error.html",{"request":request})
+
+@apirouter.get("/Studentdetails", name="student_details", response_model=ResponseTo[list[StudentResponse]])
+def get_student_details(request:Request, dal: StudentDal = Depends(get_student_dal)):
+    result = dal.get_all()
+    student_list =[StudentResponse(**student.model_dump()) for student in result]
+    return templates.TemplateResponse("studentsDetails.html",{"request":request,"data":student_list})
+
+# STUDENT PAGE (REQUEST)
 
 @apirouter.post("/submit", response_model=ResponseTo[StudentResponse])
 def create_Student_form(request:Request, form_data:StudentForm = Form(), dal: StudentDal = Depends(get_student_dal)):
@@ -51,4 +65,5 @@ def create_Student_form(request:Request, form_data:StudentForm = Form(), dal: St
     result= dal.create(data)
     value = StudentResponse(**result.model_dump())
     return templates.TemplateResponse("submitform.html",{"request":request, "submit": value})
+
 
